45a54c6fdc3e9f66159df9e9213b8c57dd7c7138
basic-html.css <basic-html.css.in>
2025-07-28 19:28:16 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,3 @@
-
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import Navbar from './component/navbar'
