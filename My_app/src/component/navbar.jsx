044f98416e8b0a083cc21a821875e01db0bb11cf
basic-html.css <basic-html.css.in>
2025-08-16 20:31:12 +0530

    update

--- My_app/src/component/navbar.jsx
+++ My_app/src/component/navbar.jsx
@@ -6,19 +6,19 @@ const Navbar = () => {
   const router = useNavigate();
   return (
     <>
-      {/* <button onClick={() => router("/")}>Home</button> */}
-      {/* <button onClick={() => router("/login")}>Login</button> */}
-      {/* <button onClick={() => router("/register")}>Register</button> */}
-      {/* <button onClick={() => router("/useffect")}>UseEffect</button> */}
-      {/* <button onClick={() => router("/usestate")}>UseState</button> */}
-      {/* <button onClick={() => router("/useparams")}>UseParams</button> */}
-      {/* <button onClick={() => router("/todo")}>Todo</button> */}
-      {/* <button onClick={() => router("/Notfound")}>NotFound</button> */}
-      {/* <button onClick={() => router("/fakestore")}>Fetch Products</button> */}
-      {/* <button onClick={() => router("/delete")}>Delete</button> */}
+      <button onClick={() => router("/")}>Home</button>
+      <button onClick={() => router("/login")}>Login</button>
+      <button onClick={() => router("/register")}>Register</button>
+      <button onClick={() => router("/useffect")}>UseEffect</button>
+      <button onClick={() => router("/usestate")}>UseState</button>
+      <button onClick={() => router("/useparams")}>UseParams</button>
+      <button onClick={() => router("/todo")}>Todo</button>
+      <button onClick={() => router("/Notfound")}>NotFound</button>
+      <button onClick={() => router("/fakestore")}>Fetch Products</button>
+      <button onClick={() => router("/delete")}>Delete</button>
       <button onClick={() => router("/products")}>Products</button>
-      {/* <button onClick={() => router("/productsInfo/1")}>Product Info</button> */}
-      {/* <button onClick={() => router("/mycart")}>My Cart</button> */}
+      <button onClick={() => router("/productsInfo/1")}>Product Info</button>
+      <button onClick={() => router("/mycart")}>My Cart</button>
       <button onClick={() => router("/UseMemo")}>UseMemo</button>
       <button onClick={() => router("/UseCallback")}>UseCallback</button>
       <button onClick={() => router("/UseRef")}>UseRef</button>
